081f9ab8ad992387082cf12c5fb4afd85e29cf92
tastysoapwater <gnslwkd13@gachon.ac.kr>
2025-09-18 00:59:23 +0900

    보호자/피보호자 웹 상 구현완료 포스트맨 테스트 성공 앱이랑 연결 후 테스트 코드 수정 필요

--- src/main/java/com/Guard/Back/Config/SecurityConfig.java
+++ src/main/java/com/Guard/Back/Config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**", "/api/relationship/**").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
